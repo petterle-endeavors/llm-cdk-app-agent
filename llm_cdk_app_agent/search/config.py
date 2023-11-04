81a1cd6c017fd925b3314ef96b0dc0b794833761
mertbozkir <mert.bozkirr@gmail.com>
2023-11-04 05:41:29 +0000

    mono function for vectorstore creation

--- llm_cdk_app_agent/search/config.py
+++ llm_cdk_app_agent/search/config.py
@@ -5,13 +5,13 @@ from langchain.vectorstores import Pinecone
 INDEX_NAME = "llm-cdk-agent"
 
 
-def index_list() -> List[str]:
+def indexList():
     return pinecone.list_indexes()
 
 
-def create_index(index_name: str = INDEX_NAME, dimension: int = 1536, metric: str = "cosine"):
+def vectorStore(index_name: str = INDEX_NAME, dimension: int = 1536, metric: str = "cosine"):
     # check before creating
-    if index_name not in index_list():
+    if index_name not in indexList():
         # index not existed. Create a new index
         pinecone.create_index(
             name=index_name,
@@ -21,24 +21,10 @@ def create_index(index_name: str = INDEX_NAME, dimension: int = 1536, metric: st
         print(f"created a new index {index_name}")
     else:
         print(f"{index_name} index existed. skip creating.")
+        index = pinecone.Index(index_name)
+        index_stats_response = index.describe_index_stats()
+        vector_count = index_stats_response.total_vector_count
+        print(f"total_vector_count is {vector_count}")
 
-
-def insert(data: List[Document], embeddings: OpenAIEmbeddings, index=INDEX_NAME) -> Pinecone:
-    return Pinecone.from_documents(data, embedding=embeddings, index_name=index)
-
-
-def need_text_embedding():
-    need_text_embedding = False
-    index = pinecone.Index(INDEX_NAME)
-    index_stats_response = index.describe_index_stats()
-    # Example of index_stats_reponse
-    # index_stats_response is {'dimension': 1536,
-    # 'index_fullness': 0.0,
-    # 'namespaces': {'': {'vector_count': 532}},
-    # 'total_vector_count': 532}
-    vector_count = index_stats_response.total_vector_count
-    print(f"total_vector_count is {vector_count}")
-
-    if vector_count == 0:
-        need_text_embedding = True
-    return need_text_embedding
+        return index
+    return pinecone.Index(index_name)
